dfdcd5cafc2ee0d9736a73dc9c37e115eceb46cf
bryx <bryxph@gmail.com>
2024-11-02 00:53:59 +0800

    Updated Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import logo from "../assets/bryanTiamzonLogo.png"
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa"
-import { FaInstagram } from "react-icons/fa"
+import logo from "../assets/bryanTiamzonLogo.png";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-    return <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" />
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text">
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-        </div>
-    </nav>
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img src={logo} alt="logo" style={{ height: '200px', width: '200px' }} />
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4 text-3xl">
+                <FaLinkedin />
+                <FaGithub />
+                <FaInstagram />
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
