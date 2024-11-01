5babb1d81c930671d4e0437fe485c8ebfbbe8703
bryx <bryxph@gmail.com>
2024-11-02 00:41:53 +0800

    Updated prototype navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="relative h-screen w-screen bg-slate-950 overflow-hidden">
+        <div className="absolute inset-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
+
+        {/* Main Content */}
+        <div className="container mx-auto px-8">
+          <Navbar />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { FaGithub } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa"
 
 const Navbar = () => {
-    return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    return <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
             <img src={logo} alt="logo" />
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <div className="m-8 flex items-center justify-center gap-4 text">
             <FaLinkedin />
             <FaGithub />
             <FaInstagram />
