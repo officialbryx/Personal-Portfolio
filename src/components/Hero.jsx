eec5d1a92a6fd9016fa305daf71d73953ae7bc23
bryx <bryxph@gmail.com>
2024-11-02 01:30:26 +0800

    Hero Section Prototype

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { HERO_CONTENT } from "../constants/index";
+import profilePic from "../assets/profilePic.jpg";
+
+const Hero = () => {
+    return (
+        <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl">Bryan Tiamzon</h1>
+                        <span className="bg-gradient-to-r from-red-500 via-purple-500 to-indigo-700 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Data Scientist
+                        </span>
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img
+                            src={profilePic}
+                            alt="Bryan Tiamzon"
+                            className="border"
+                            style={{ borderRadius: "20px" }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img src={logo} alt="logo" style={{ height: '200px', width: '200px' }} />
+                <img src={logo} alt="logo" style={{ height: '170px', width: '170px' }} />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-3xl">
                 <FaLinkedin />
