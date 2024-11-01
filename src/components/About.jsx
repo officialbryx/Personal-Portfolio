8584da3973feebdc17a07290a9ce0693cd26d25f
bryx <bryxph@gmail.com>
2024-11-02 02:08:53 +0800

    again mb

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                 </div>
                 <div className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:justify-start">
-                        <p>{ABOUT_TEXT}</p>
+                        <p classname="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
                     </div>
                 </div>
             </div>
