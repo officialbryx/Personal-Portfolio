586b7191d7f561cc074aa08f352e2e8e4c190f6f
bryx <bryxph@gmail.com>
2024-11-02 14:56:39 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll'
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
-            <div className="flex flex-shrink-0 items-center">
+            <div className="flex flex-shrink-0 items-center -ml-12">
                 <img src={logo} alt="logo" style={{ height: '170px', width: '170px' }} />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
